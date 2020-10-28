f816fa50426424675193181be301311b69160049
CantThinkOfAnUsername <pranjalsingh.tsrs@gmail.com>
2020-10-28 16:37:12 +0530

    ConstraintBP

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,7 +38,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    constrainedLog=new Log(200,100,100,PI/2);
+    chain = new Chain(bird.body, constrainedLog.body)
 }
 
 function draw(){
@@ -61,4 +65,9 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+    chain.display();
+
+    strokeWeight(4);
+    line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,constrainedLog.body.position.y);
 }
